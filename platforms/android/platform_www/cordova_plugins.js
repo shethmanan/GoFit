4f158eced1a01bae83819a1484697e79d6847cc6
dVp007 <dharmilp03@gmail.com>
2016-02-10 21:30:14 +0530

    'dharmil'

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -5,13 +5,22 @@ module.exports = [
         "id": "cordova-plugin-whitelist.whitelist",
         "pluginId": "cordova-plugin-whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-pedometer/www/pedometer.js",
+        "id": "cordova-plugin-pedometer.Pedometer",
+        "pluginId": "cordova-plugin-pedometer",
+        "clobbers": [
+            "pedometer"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.2.1",
-    "cordova-plugin-crosswalk-webview": "1.5.0"
+    "cordova-plugin-crosswalk-webview": "1.5.0",
+    "cordova-plugin-pedometer": "0.4.1"
 }
 // BOTTOM OF METADATA
 });
