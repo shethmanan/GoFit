12d677acc549581a3fb62938723ec9f49d19ce5b
dVp007 <dharmilp03@gmail.com>
2016-02-22 20:08:52 +0530

    NO-USE

---
+++ platforms/android/assets/www/js/navbar-transition.js
@@ -0,0 +1,19 @@
+$("#nav-btn").click(function(){
+				$("main").toggleClass("active");
+				$("main").toggleClass("z-depth-3");
+				if($("main").hasClass("active")){
+					$("main.active").velocity({
+						translateX: "65vw",
+						translateY: "10vh",
+					},{easing:"linear"});
+				}
+				else
+				{
+					$("main").velocity({
+						translateX: "0px",
+						translateY: "0px",
+						height:"100 vh",
+						width:"100 vw"
+					},{easing:"linear"});	
+				}
+			});
