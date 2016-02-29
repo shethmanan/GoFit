99cb85605f453a46e1930d92065f55fc138f73f1
Manan Sheth <shethmanan1997@gmail.com>
2016-02-29 22:29:28 +0530

    logo-added

---
+++ platforms/android/assets/www/js/reminder.js
@@ -0,0 +1,105 @@
+/*----------------------card over ---------------------------------------------------------------*/
+		$(document).ready(function(){
+			if(localStorage.length !=0 )
+			{
+				console.log(localStorage.length);
+
+				for (var i = 0; i < localStorage.length; i++) {
+					key = localStorage.getItem("notify_"+i);
+					value = jQuery.parseJSON(key);
+		/*-------------------card ------------------------------------------------*/ 
+		var x='<div class="row">'
+							x+='<div class="col s12">'
+							x+='<div class="card-small blue darken-2" id="'+value.id+'">'
+								x+=	'<div class="row no-margin">'
+									x+='<div class="col s4">'
+											x+='<i class="large material-icons amber-text center-align">assignment_late</i>'
+										x+='</div>'
+										x+='<div class="col s8 no-padding no-margin">'
+										x+='<div class="row">'
+												x+='<div class="col s10">'
+													x+='<h5 class="amber-text" style="margin-bottom:0px;">'+value.title+'</h5>'
+												x+='</div>'
+												x+='<div class="col s2 no-padding right-align">'
+													x+='<i class="material-icons deep-orange-text" onclick="removeCard(this)">cancel</i>'
+												x+='</div>'
+											x+='</div>'
+											x+='<div class="row no-margin">'
+												x+='<div class="col s12 amber-text">'
+													x+='<h5 class="no-margin no-padding">Every : '+value.time+'</h5>'
+												x+='</div>'
+											x+='</div>'
+										x+='</div>'
+									x+='</div>'	
+								x+='</div>'
+							x+='</div>'
+						x+='</div>';
+					$(".d-card").append(x);
+				}
+			}
+			$('.modal-trigger').click(function(){
+				removeAttr('main');
+				$('#reminder').openModal();
+			})
+			$('select').material_select();
+		});
+		$('#submit_remind').on('click',function(){
+			closeModal("#reminder");
+		});
+		var title;
+		var time;
+		var freq;
+		var i=localStorage.length;
+		function storeData(){
+				title=$("#title_text").val();
+				time=$("#time_text").val();
+				if(title=="" || time==-1) return false;
+				time+=" mins";
+				var notify={id:i,title:title,time:time};
+				localStorage.setItem("notify_"+i,JSON.stringify(notify));
+				return true;
+		}
+				function closeModal(data)
+				{
+					$(data).closeModal();
+					if(storeData()){
+
+		/*-------------------card ------------------------------------------------ */
+		var x='<div class="row">'
+							x+='<div class="col s12">'
+							x+='<div class="card-small blue darken-2" id="'+i+'">'
+								x+=	'<div class="row no-margin">'
+									x+='<div class="col s4">'
+											x+='<i class="large material-icons amber-text center-align">assignment_late</i>'
+										x+='</div>'
+										x+='<div class="col s8 no-padding no-margin">'
+										x+='<div class="row">'
+												x+='<div class="col s10">'
+													x+='<h5 class="amber-text" style="margin-bottom:0px;">'+title+'</h5>'
+												x+='</div>'
+												x+='<div class="col s2 no-padding right-align">'
+													x+='<i class="material-icons deep-orange-text" onclick="removeCard(this)">cancel</i>'
+												x+='</div>'
+											x+='</div>'
+											x+='<div class="row no-margin">'
+												x+='<div class="col s12 amber-text">'
+													x+='<h5 class="no-margin no-padding">Every : '+time+'</h5>'
+												x+='</div>'
+											x+='</div>'
+										x+='</div>'
+									x+='</div>'	
+								x+='</div>'
+							x+='</div>'
+						x+='</div>';	
+					$(".d-card").append(x);
+					i++;
+				}
+				return false;
+			};
+			function removeCard(data)
+			{
+				var card = $(data).closest("div.card-small");
+				var key = "notify_"+card.attr("id");
+				localStorage.removeItem(key);
+				card.closest("div.row").remove();
+			}
